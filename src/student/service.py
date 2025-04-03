943fe5fc20383fe7a55122701d6015819cdfecd4
bhuvanesh <bhuvanesh.msu@gmail.com>
2025-04-03 11:14:09 -0400

    - added method to add a student to DB.

---
+++ src/student/service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from src import models
+from . import schemas
+
+
+async def create_student_user(student_data: schemas.Student, async_session: AsyncSession) -> int:
+    """
+    This method creates user in DB
+    :param student_data: The student data
+    :param async_session: The DB session
+    :return: Created Student ID
+    """
+    new_user = models.Student(first_name=student_data.first_name, last_name=student_data.last_name,
+                    email=student_data.email, dob=student_data.dob, phone_number=student_data.phone_number)
+    async_session.add(new_user)
+    await async_session.commit()
+    return new_user.id
